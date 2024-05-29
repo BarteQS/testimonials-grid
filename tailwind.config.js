3ab8debe2163e889d108309f3f9dc3fe3c876b55
Bartosz Utratny <utratny.bartosz@gmail.com>
2024-05-29 02:40:09 +0200

    Finished the mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'moderateViolet': '#7541c8',
+      'darkGrayishBlue': '#48556a',
+      'darkBlackishBlue': '#19212e',
+      'lightGray': '#cfcfcf',
+      'lightGrayishBlue': '#ecf2f8',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      barlow: ['Barlow Semi Condensed', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
